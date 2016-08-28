65d9c86fbf4980d7e468f8f6de5e195b03f3c1d4
Nicole Url <nicole.url.4@gmail.com>
2016-08-28 13:58:41 -0700

    Finished Leap weekend warrior, pushing a week later

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -1,8 +1,6 @@
 def leap_year?(year)
-  if year % 400 == 0
+  if (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)
     return true
-  elsif (year % 4 == 0) && (year % 100 != 0)
-    return true 
   else
     return false
   end
